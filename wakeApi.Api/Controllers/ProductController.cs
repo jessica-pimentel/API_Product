3e31a690287b4cb478146242ae96a9485b318cf6
jessica-pimentel <jppimentel96@hotmail.com>
2024-05-16 21:05:00 -0300

    [HOTFIX] correções no controller, validação no serviço e tirado try catch do controller.

--- wakeApi.Api/Controllers/ProductController.cs
+++ wakeApi.Api/Controllers/ProductController.cs
@@ -4,7 +4,7 @@ using wakeDomain.Domain.Models;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductController : ControllerBase
+public class ProductController : BaseController
 {
     private readonly IProductService _productService;
 
@@ -13,35 +13,76 @@ public class ProductController : ControllerBase
             _productService = productService;
         }
 
+        [HttpPost("register")]
+        public async Task<ActionResult> Add([FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+                return CustomResponse(ModelState);
+
+            var result = await _productService.Add(product);
+
+            if (result)
+                return CustomResponse($"Produto {product.ProductName} com preço {product.ProductPrice} adicionado com sucesso!");
+
+            return CustomResponseError("Erro ao tentar adicionar produto.");
+
+        }
+
+        [HttpPut("update/{productId}")]
+        public async Task<IActionResult> Update(Guid productId, [FromBody] Product product)
+        {
+            if (productId != product.ProductId)
+                return CustomResponseError("Produto não encontrado.");
+
+            if (!ModelState.IsValid)
+                return CustomResponse(ModelState);
+
+            var updatedProduct = await _productService.Update(product);
+
+            if (updatedProduct == null)
+                return CustomResponseError("Produto não encontrado.");
+
+            return CustomResponse("Produto atualizado com sucesso!");
+        }
+
+        [HttpDelete("delete/{productId}")]
+        public async Task<IActionResult> Delete(Guid productId)
+        {
+            var result = await _productService.Delete(productId);
+            if (result)
+                return CustomResponse("Produto deletado com sucesso!");
+
+            return CustomResponseError("Produto não encontrad.");
+        }
+
         [HttpGet("getAll")]
         public async Task<ActionResult<IEnumerable<Product>>> GetAll()
         {
             var products = await _productService.GetAll();
-            return Ok(products);
+            return CustomResponse(products);
         }
 
-        [HttpGet("getById")]
-        public async Task<ActionResult<Product>> GetById(Guid id)
+        [HttpGet("getById/{productId}")]
+        public async Task<ActionResult<Product>> GetById(Guid productId)
         {
-            var product = await _productService.GetById(id);
+            var product = await _productService.GetById(productId);
             if (product == null)
-            {
-                return NotFound();
-            }
-            return Ok(product);
+                return CustomResponseError("Produto não encontrado.");
+
+            return CustomResponse(product);
         }
 
         [HttpGet("orderByType")]
-        public async Task<ActionResult<IEnumerable<Product>>> OrderByType([FromQuery] string field)
+        public async Task<ActionResult<IEnumerable<Product>>> OrderByType([FromQuery] string type)
         {
-            var products = await _productService.OrderByType(field);
-            return Ok(products);
+            var products = await _productService.OrderByType(type);
+            return CustomResponse(products); ;
         }
 
         [HttpGet("searchByName")]
         public async Task<ActionResult<IEnumerable<Product>>> SearchByName([FromQuery] string name)
         {
             var products = await _productService.SearchByName(name);
-            return Ok(products);
+            return CustomResponse(products); ;
         }
 }
